356c89df244290fa2fd5877e11bdd23a334efcc7
SimplexTech <info@simplextech.net>
2019-09-12 17:03:04 -0400

    Update isy-net.py
    Initial check_parms and readiness for nodeserver.  Now to work on cleanup

--- isy-net.py
+++ isy-net.py
@@ -13,128 +13,45 @@ import sys
 import time
 import PyISY
 import re
-"""
-Import the polyglot interface module. This is in pypy so you can just install it
-normally. Replace pip with pip3 if you are using python3.
-
-Virtualenv:
-pip install polyinterface
-
-Not Virutalenv:
-pip install polyinterface --user
-
-*I recommend you ALWAYS develop your NodeServers in virtualenv to maintain
-cleanliness, however that isn't required. I do not condone installing pip
-modules globally. Use the --user flag, not sudo.
-"""
 
 LOGGER = polyinterface.LOGGER
-"""
-polyinterface has a LOGGER that is created by default and logs to:
-logs/debug.log
-You can use LOGGER.info, LOGGER.warning, LOGGER.debug, LOGGER.error levels as needed.
-"""
+
 
 class Controller(polyinterface.Controller):
-    """
-    The Controller Class is the primary node from an ISY perspective. It is a Superclass
-    of polyinterface.Node so all methods from polyinterface.Node are available to this
-    class as well.
-
-    Class Variables:
-    self.nodes: Dictionary of nodes. Includes the Controller node. Keys are the node addresses
-    self.name: String name of the node
-    self.address: String Address of Node, must be less than 14 characters (ISY limitation)
-    self.polyConfig: Full JSON config dictionary received from Polyglot for the controller Node
-    self.added: Boolean Confirmed added to ISY as primary node
-    self.config: Dictionary, this node's Config
-
-    Class Methods (not including the Node methods):
-    start(): Once the NodeServer config is received from Polyglot this method is automatically called.
-    addNode(polyinterface.Node, update = False): Adds Node to self.nodes and polyglot/ISY. This is called
-        for you on the controller itself. Update = True overwrites the existing Node data.
-    updateNode(polyinterface.Node): Overwrites the existing node data here and on Polyglot.
-    delNode(address): Deletes a Node from the self.nodes/polyglot and ISY. Address is the Node's Address
-    longPoll(): Runs every longPoll seconds (set initially in the server.json or default 10 seconds)
-    shortPoll(): Runs every shortPoll seconds (set initially in the server.json or default 30 seconds)
-    query(): Queries and reports ALL drivers for ALL nodes to the ISY.
-    getDriver('ST'): gets the current value from Polyglot for driver 'ST' returns a STRING, cast as needed
-    runForever(): Easy way to run forever without maxing your CPU or doing some silly 'time.sleep' nonsense
-                  this joins the underlying queue query thread and just waits for it to terminate
-                  which never happens.
-    """
     def __init__(self, polyglot):
-        """
-        Optional.
-        Super runs all the parent class necessities. You do NOT have
-        to override the __init__ method, but if you do, you MUST call super.
-        """
         super(Controller, self).__init__(polyglot)
-        self.name = 'Template Controller'
+        self.name = 'ISY-Net Controller'
         self.poly.onConfig(self.process_config)
+        self.user = None
+        self.password = None
+        self.ipaddress = None
+        self.port = None
         self.isy = None
 
     def start(self):
-        """
-        Optional.
-        Polyglot v2 Interface startup done. Here is where you start your integration.
-        This will run, once the NodeServer connects to Polyglot and gets it's config.
-        In this example I am calling a discovery method. While this is optional,
-        this is where you should start. No need to Super this method, the parent
-        version does nothing.
-        """
-        LOGGER.info('Started Template NodeServer')
+        LOGGER.info('Started ISY-Net NodeServer')
         self.removeNoticesAll()
-        # self.addNotice({'hello': 'Hello Friends!'})
-        self.check_params()
-
-        self.isy = PyISY.ISY('192.168.1.69', '80', 'admin', 'admin')
-
-        if self.isy.connected:
-            LOGGER.info('ISY Connected: True')
-            self.isy.auto_update = True
-            self.discover()
-        else:
-            LOGGER.info('ISY Connected: False')
-        # self.poly.add_custom_config_docs("<b>And this is some custom config data</b>")
+        if self.check_params():
+            self.isy = PyISY.ISY(self.ipaddress, '80', self.user, self.password)
+            if self.isy.connected:
+                LOGGER.info('ISY Connected: True')
+                self.isy.auto_update = True
+                self.discover()
+            else:
+                LOGGER.info('ISY Connected: False')
 
     def shortPoll(self):
-        """
-        Optional.
-        This runs every 10 seconds. You would probably update your nodes either here
-        or longPoll. No need to Super this method the parent version does nothing.
-        The timer can be overriden in the server.json.
-        """
         pass
 
     def longPoll(self):
-        """
-        Optional.
-        This runs every 30 seconds. You would probably update your nodes either here
-        or shortPoll. No need to Super this method the parent version does nothing.
-        The timer can be overriden in the server.json.
-        """
         pass
 
     def query(self):
-        """
-        Optional.
-        By default a query to the control node reports the FULL driver set for ALL
-        nodes back to ISY. If you override this method you will need to Super or
-        issue a reportDrivers() to each node manually.
-        """
         self.check_params()
         for node in self.nodes:
             self.nodes[node].reportDrivers()
 
     def discover(self, *args, **kwargs):
-        """
-        Example
-        Do discovery here. Does not have to be called discovery. Called from example
-        controller start method and from DISCOVER command recieved from ISY as an exmaple.
-        """
-        self.addNode(TemplateNode(self, self.address, 'templateaddr', 'Template Node Name'))
-
         for nid in self.isy.nodes.nids:
             if re.match(r'^ZW', nid):
                 # print(self.isy.nodes[nid].type)
@@ -330,36 +247,48 @@ class Controller(polyinterface.Controller):
         pass
 
     def check_params(self):
-        """
-        This is an example if using custom Params for user and password and an example with a Dictionary
-        """
-        # self.addNotice('Hello Friends! (with key)','hello')
-        # self.addNotice('Hello Friends! (without key)')
         default_user = "YourUserName"
         default_password = "YourPassword"
+        default_ipaddress = "127.0.0.1"
+        default_port = "80"
+
         if 'user' in self.polyConfig['customParams']:
             self.user = self.polyConfig['customParams']['user']
         else:
             self.user = default_user
             LOGGER.error('check_params: user not defined in customParams, please add it.  Using {}'.format(self.user))
-            st = False
 
         if 'password' in self.polyConfig['customParams']:
             self.password = self.polyConfig['customParams']['password']
         else:
             self.password = default_password
-            LOGGER.error('check_params: password not defined in customParams, please add it.  Using {}'.format(self.password))
-            st = False
+            LOGGER.error('check_params: password not defined in customParams, please add it.  '
+                         'Using {}'.format(self.password))
+
+        if 'ipaddress' in self.polyConfig['customParams']:
+            self.ipaddress = self.polyConfig['customParams']['ipaddress']
+        else:
+            self.ipaddress = default_ipaddress
+            LOGGER.error('check_params: ip address not defined in customParams, please add it.  '
+                         'Using {}'.format(self.ipaddress))
+
+        if 'port' in self.polyConfig['customParams']:
+            self.port = self.polyConfig['customParams']['port']
+        else:
+            self.port = default_port
+            LOGGER.error('check_params: port not defined in customParams, please add it.  Using {}'.format(self.port))
 
         # Make sure they are in the params
-        self.addCustomParam({'password': self.password, 'user': self.user, 'some_example': '{ "type": "TheType", "host": "host_or_IP", "port": "port_number" }'})
+        self.addCustomParam({'password': self.password, 'user': self.user,
+                             'ipaddress': self.ipaddress, 'port': self.port })
 
         # Add a notice if they need to change the user/password from the default.
-        if self.user == default_user or self.password == default_password:
-            # This doesn't pass a key to test the old way.
-            self.addNotice('Please set proper user and password in configuration page, and restart this nodeserver')
-        # This one passes a key to test the new way.
-        self.addNotice('This is a test','test')
+        if self.user == default_user or self.password == default_password or self.ipaddress == default_ipaddress:
+            self.addNotice('Please set proper user, password and IP address/port in configuration page, '
+                           'and restart this nodeserver')
+            return False
+        else:
+            return True
 
     def remove_notice_test(self,command):
         LOGGER.info('remove_notice_test: notices={}'.format(self.poly.config['notices']))
@@ -376,23 +305,13 @@ class Controller(polyinterface.Controller):
         st = self.poly.installprofile()
         return st
 
-    """
-    Optional.
-    Since the controller is the parent node in ISY, it will actual show up as a node.
-    So it needs to know the drivers and what id it will use. The drivers are
-    the defaults in the parent Class, so you don't need them unless you want to add to
-    them. The ST and GV1 variables are for reporting status through Polyglot to ISY,
-    DO NOT remove them. UOM 2 is boolean.
-    The id must match the nodeDef id="controller"
-    In the nodedefs.xml
-    """
     id = 'controller'
     commands = {
         'QUERY': query,
         'DISCOVER': discover,
         'UPDATE_PROFILE': update_profile,
-        'REMOVE_NOTICES_ALL': remove_notices_all,
-        'REMOVE_NOTICE_TEST': remove_notice_test
+        # 'REMOVE_NOTICES_ALL': remove_notices_all,
+        # 'REMOVE_NOTICE_TEST': remove_notice_test
     }
     drivers = [{'driver': 'ST', 'value': 1, 'uom': 2}]
 
@@ -533,118 +452,12 @@ class TStatNode(polyinterface.Node):
                 }
 
 
-class TemplateNode(polyinterface.Node):
-    """
-    This is the class that all the Nodes will be represented by. You will add this to
-    Polyglot/ISY with the controller.addNode method.
-
-    Class Variables:
-    self.primary: String address of the Controller node.
-    self.parent: Easy access to the Controller Class from the node itself.
-    self.address: String address of this Node 14 character limit. (ISY limitation)
-    self.added: Boolean Confirmed added to ISY
-
-    Class Methods:
-    start(): This method is called once polyglot confirms the node is added to ISY.
-    setDriver('ST', 1, report = True, force = False):
-        This sets the driver 'ST' to 1. If report is False we do not report it to
-        Polyglot/ISY. If force is True, we send a report even if the value hasn't changed.
-    reportDrivers(): Forces a full update of all drivers to Polyglot/ISY.
-    query(): Called when ISY sends a query request to Polyglot for this specific node
-    """
-    def __init__(self, controller, primary, address, name):
-        """
-        Optional.
-        Super runs all the parent class necessities. You do NOT have
-        to override the __init__ method, but if you do, you MUST call super.
-
-        :param controller: Reference to the Controller class
-        :param primary: Controller address
-        :param address: This nodes address
-        :param name: This nodes name
-        """
-        super(TemplateNode, self).__init__(controller, primary, address, name)
-
-    def start(self):
-        """
-        Optional.
-        This method is run once the Node is successfully added to the ISY
-        and we get a return result from Polyglot. Only happens once.
-        """
-        self.setDriver('ST', 1)
-        pass
-
-    def setOn(self, command):
-        """
-        Example command received from ISY.
-        Set DON on TemplateNode.
-        Sets the ST (status) driver to 1 or 'True'
-        """
-        self.setDriver('ST', 1)
-
-    def setOff(self, command):
-        """
-        Example command received from ISY.
-        Set DOF on TemplateNode
-        Sets the ST (status) driver to 0 or 'False'
-        """
-        self.setDriver('ST', 0)
-
-    def query(self):
-        """
-        Called by ISY to report all drivers for this node. This is done in
-        the parent class, so you don't need to override this method unless
-        there is a need.
-        """
-        self.reportDrivers()
-
-    "Hints See: https://github.com/UniversalDevicesInc/hints"
-    hint = [1,2,3,4]
-    drivers = [{'driver': 'ST', 'value': 0, 'uom': 2}]
-    """
-    Optional.
-    This is an array of dictionary items containing the variable names(drivers)
-    values and uoms(units of measure) from ISY. This is how ISY knows what kind
-    of variable to display. Check the UOM's in the WSDK for a complete list.
-    UOM 2 is boolean so the ISY will display 'True/False'
-    """
-    id = 'templatenodeid'
-    """
-    id of the node from the nodedefs.xml that is in the profile.zip. This tells
-    the ISY what fields and commands this node has.
-    """
-    commands = {
-                    'DON': setOn, 'DOF': setOff
-                }
-    """
-    This is a dictionary of commands. If ISY sends a command to the NodeServer,
-    this tells it which method to call. DON calls setOn, etc.
-    """
-
 if __name__ == "__main__":
     try:
-        polyglot = polyinterface.Interface('Template')
-        """
-        Instantiates the Interface to Polyglot.
-        The name doesn't really matter unless you are starting it from the
-        command line then you need a line Template=N
-        where N is the slot number.
-        """
+        polyglot = polyinterface.Interface('ISYNet')
         polyglot.start()
-        """
-        Starts MQTT and connects to Polyglot.
-        """
         control = Controller(polyglot)
-        """
-        Creates the Controller Node and passes in the Interface
-        """
         control.runForever()
-        """
-        Sits around and does nothing forever, keeping your program running.
-        """
     except (KeyboardInterrupt, SystemExit):
         polyglot.stop()
         sys.exit(0)
-        """
-        Catch SIGTERM or Control-C and exit cleanly.
-        """
